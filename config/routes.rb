d35e24a941ceff45648ff9fcc368953be493fc09
Matt Hansen <matt.hansen@oregonstate.edu>
2012-04-27 08:01:28 -0700

    fix typo in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # Redmine Bulk User Assignment: redmine plugin to simplify assigning users to projects
 # Copyright (C) 2012  Oregon State University
 
-#custom routes for redmine_assign_project_roles
+#custom routes for redmine_bulk_user_assignment
 ActionController::Routing::Routes.draw do |map|
 
-  map.with_options :controller => 'bulk_user_assignment' do |bukl_assign_routes|
+  map.with_options :controller => 'bulk_user_assignment' do |bulk_assign_routes|
     bulk_assign_routes.connect "bulk_user_assignment", :conditions => { :method => :get }, :action => 'index'
     bulk_assign_routes.connect "bulk_user_assignment/assign", :conditions => { :method => :post }, :action => 'assign'
   end
