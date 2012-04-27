954b735caa00074d4fab587ea1a67d2c967423f9
Matt Hansen <matt.hansen@oregonstate.edu>
2012-04-27 07:48:08 -0700

    rename plugin to Bulk User Assignment in init and config files

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
-# Redmine Assign Project Roles: redmine plugin to simplify assigning users to projects
+# Redmine Bulk User Assignment: redmine plugin to simplify assigning users to projects
 # Copyright (C) 2012  Oregon State University
 
 #custom routes for redmine_assign_project_roles
 ActionController::Routing::Routes.draw do |map|
 
-  map.with_options :controller => 'assign_project_roles' do |assign_routes|
-    assign_routes.connect "assign_project_roles", :conditions => { :method => :get }, :action => 'index'
-    assign_routes.connect "assign_project_roles/assign", :conditions => { :method => :post }, :action => 'assign'
+  map.with_options :controller => 'bulk_user_assignment' do |bukl_assign_routes|
+    bulk_assign_routes.connect "bulk_user_assignment", :conditions => { :method => :get }, :action => 'index'
+    bulk_assign_routes.connect "bulk_user_assignment/assign", :conditions => { :method => :post }, :action => 'assign'
   end
 
 end
--- init.rb
+++ init.rb
@@ -1,21 +1,21 @@
-# Redmine Assign Project Roles: redmine plugin to simplify assigning users to projects
+# Redmine Bulk User Assignment: redmine plugin to simplify assigning users to projects
 # Copyright (C) 2012  Oregon State University
 
 require 'redmine'
 
-Redmine::Plugin.register :redmine_assign_project_roles do
-  name 'Redmine Assign Project Roles'
+Redmine::Plugin.register :redmine_bulk_user_assignment do
+  name 'Redmine Bulk User Assignment'
   author 'Matt Hansen'
   description 'This plugin provides an interface for assigning multiple users to project(s) with specified role(s)'
   version '1.0.0'
   url ''
   author_url ''
 
-  permission :assign_project_roles, :assign_project_roles => :index
+  permission :bulk_user_assignment, :bulk_user_assignment => :index
 
-  menu :admin_menu, :assign_project_roles,
-                          {:controller => 'assign_project_roles', :action => 'index'},
-                          :caption => :redmine_assign_project_roles,
+  menu :admin_menu, :bulk_user_assignment,
+                          {:controller => 'bulk_user_assignment', :action => 'index'},
+                          :caption => :redmine_bulk_user_assignment,
                           :html => { :class => 'icon roles' }
 
 end
