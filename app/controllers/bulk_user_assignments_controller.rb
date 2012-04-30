2c4bc65abce6b5703688e32a6f307483b054c1e3
Matt Hansen <matt.hansen@oregonstate.edu>
2012-04-30 08:58:22 -0700

    add license

--- app/controllers/bulk_user_assignments_controller.rb
+++ app/controllers/bulk_user_assignments_controller.rb
@@ -1,5 +1,27 @@
 # Redmine Bulk User Assignments: redmine plugin to simplify assigning users to projects
 # Copyright (C) 2012  Oregon State University
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+# To contact us, go to http://oregonstate.edu/cws/contact and fill out the contact form.
+#
+# Alternatively mail us at:
+#
+# Oregon State University
+# Central Web Services
+# 121 The Valley Library
+# Corvallis, OR 97331
 
 # Bulk User Assignments Controller
 #
