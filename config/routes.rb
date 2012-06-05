62b2658f0685d8c417ef3db154aa53f509ebaad5
Pedro Medeiros <pedro.medeiros@ebc.com.br>
2012-06-05 16:38:23 -0300

    update to use on redmine 2.0.X

--- config/routes.rb
+++ config/routes.rb
@@ -24,11 +24,8 @@
 # Corvallis, OR 97331
 
 #custom routes for redmine_bulk_user_assignments
-ActionController::Routing::Routes.draw do |map|
 
-  map.with_options :controller => 'bulk_user_assignments' do |bulk_assign_routes|
-    bulk_assign_routes.connect "bulk_user_assignments", :conditions => { :method => :get }, :action => 'index'
-    bulk_assign_routes.connect "bulk_user_assignments/assign", :conditions => { :method => :post }, :action => 'assign'
+  scope "bulk_user_assignments" do
+    get "/", :controller => 'bulk_user_assignments', :action => 'index'
+    post "/assign", :controller => 'bulk_user_assignments', :action => 'assign'
   end
-
-end
